97ab8ec2f98f787542dde636013c99699a2ca334
Harry Linrui XU <xulr0820@hotmail.com>
2024-04-15 13:19:45 +0200

    initial commit

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/SpareStiBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpareStiBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpareStiBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/SpareStiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpareStiBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
