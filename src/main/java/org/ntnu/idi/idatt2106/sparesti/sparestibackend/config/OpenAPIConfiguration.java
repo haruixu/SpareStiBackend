c41b8fa833507173558201bb46262248417f2af4
Harry Linrui XU <xulr0820@hotmail.com>
2024-04-15 22:43:05 +0200

    feat: Add spotless dependency for checking and formatting code in pom and .gilab-ci.yml

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/config/OpenAPIConfiguration.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/config/OpenAPIConfiguration.java
@@ -1,3 +1,4 @@
+/* (C)2024 */
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
@@ -11,21 +12,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenAPIConfiguration {
 
-  @Bean
-  public OpenAPI defineOpenApi() {
-    Server server = new Server();
-    server.setUrl("http://localhost:8080");
-    server.setDescription("SpareSti development API");
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("SpareSti development API");
 
-    Contact myContact = new Contact();
-    myContact.setName("Harry Xu");
-    myContact.setEmail("xulr0820@hotmail.com");
+        Contact myContact = new Contact();
+        myContact.setName("Harry Xu");
+        myContact.setEmail("xulr0820@hotmail.com");
 
-    Info information = new Info()
-        .title("Budgeting and saving system")
-        .version("1.0")
-        .description("This API exposes endpoints to budget and save money")
-        .contact(myContact);
-    return new OpenAPI().info(information).servers(List.of(server));
-  }
+        Info information =
+                new Info()
+                        .title("Budgeting and saving system")
+                        .version("1.0")
+                        .description("This API exposes endpoints to budget and save money")
+                        .contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
 }
