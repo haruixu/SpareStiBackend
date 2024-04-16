918cb36d14daa8dbfd5f77050de0bc4021602995
harui <xulr0820@hotmail.com>
2024-04-16 20:50:04 +0200

    fix: prevent test bugs by using separate application config file for testing

--- src/test/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/SpareStiBackendApplicationTests.java
+++ src/test/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/SpareStiBackendApplicationTests.java
@@ -2,8 +2,12 @@ package org.ntnu.idi.idatt2106.sparesti.sparestibackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@TestPropertySource(locations= "classpath:application-test.yml")
+@ActiveProfiles("test")
 class SpareStiBackendApplicationTests {
 
   @Test
