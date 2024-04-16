727ee7f4652392d3d371ea635924f4c34ab6402a
harui <xulr0820@hotmail.com>
2024-04-16 20:52:06 +0200

    format

--- src/test/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/SpareStiBackendApplicationTests.java
+++ src/test/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/SpareStiBackendApplicationTests.java
@@ -2,16 +2,14 @@ package org.ntnu.idi.idatt2106.sparesti.sparestibackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations= "classpath:application-test.yml")
+@TestPropertySource(locations = "classpath:application-test.yml")
 @ActiveProfiles("test")
 class SpareStiBackendApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
-
+    @Test
+    void contextLoads() {}
 }
