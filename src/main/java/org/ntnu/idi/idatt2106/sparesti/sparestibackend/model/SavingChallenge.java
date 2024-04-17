af0b193d0927890ca9779c0635b4976d4a5ff2c6
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-17 12:34:18 +0200

    build: new SavingChallenge and SavingGoal classes

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/SavingChallenge.java
@@ -0,0 +1,35 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Embeddable
+@Data
+public class SavingChallenge {
+
+  @Column(nullable = false)
+  @NotNull
+  private String title;
+
+  @Column(nullable = false)
+  @NotNull
+  @ColumnDefault("0.00")
+  private BigDecimal saved;
+
+  @Column(nullable = false)
+  @NotNull
+  private BigDecimal goal;
+
+  @CreationTimestamp
+  private LocalDateTime createdOn;
+
+  @Transient
+  private double completion;
+}
---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/SavingGoal.java
@@ -0,0 +1,36 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Embeddable
+@Data
+public class SavingGoal {
+
+  @Column(nullable = false)
+  @NotNull
+  private String title;
+
+  @Column(nullable = false)
+  @NotNull
+  @ColumnDefault("0.00")
+  private BigDecimal saved;
+
+  @Column(nullable = false)
+  @NotNull
+  private BigDecimal goal;
+
+  @CreationTimestamp
+  private LocalDateTime createdOn;
+
+  @Transient
+  private double completion;
+}
