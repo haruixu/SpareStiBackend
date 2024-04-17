3904de7944cd07a4f9643249147e8ee1ea558fb9
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-17 13:36:31 +0200

    build: enum classes for user experience and motivation

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/enums/UserExperience.java
@@ -0,0 +1,14 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserExperience {
+  VERY_HIGH(5), HIGH(4), MIDDLE(3), LOW(2), VERY_LOW(1);
+
+  private final int val;
+
+  UserExperience(int val) {
+    this.val = val;
+  }
+}
