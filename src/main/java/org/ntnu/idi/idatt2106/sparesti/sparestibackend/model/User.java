a1a422c3b7e4c1902706f4ef68df248e67361209
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-17 18:51:34 +0200

    fix: format

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/User.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/User.java
@@ -18,7 +18,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name ="USERS")
+@Table(name = "USERS")
 public class User implements UserDetails {
 
     @Id
