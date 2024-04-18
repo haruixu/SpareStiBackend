2ae501c36ef7f85acca20a25e5217b48cf067744
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-18 10:17:25 +0200

    docs: javadoc

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/enums/ChallengeType.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/enums/ChallengeType.java
@@ -3,16 +3,18 @@ package org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Represents different types of challenges and their associated monetary values for each consumption.
+ */
 @Getter
 @AllArgsConstructor
 public enum ChallengeType {
-  CIGAR(20),
-  CLOTHING(200),
-  COFFEE(20),
-  SNUFF(20),
-  SNACKS(20),
-  TRANSPORTATION(20);
+    CIGAR(20),
+    CLOTHING(200),
+    COFFEE(20),
+    SNUFF(20),
+    SNACKS(20),
+    TRANSPORTATION(20);
 
-  private final int val;
-
+    private final int val;
 }
