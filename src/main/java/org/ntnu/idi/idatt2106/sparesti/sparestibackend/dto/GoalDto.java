44ca499f5928776985b6899c6d5d857270f89e63
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-18 12:00:12 +0200

    build: implement goal and challenge dtos

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/dto/GoalDto.java
@@ -0,0 +1,25 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * DTO for {@link org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.Goal}
+ */
+@Value
+@Builder
+// @JsonIgnoreProperties(ignoreUnknown = true)
+public class GoalDto implements Serializable {
+    @NotNull @NotEmpty @NotBlank String title;
+    @NotNull @PositiveOrZero BigDecimal saved;
+    @NotNull @PositiveOrZero BigDecimal target;
+    @NotNull @NotEmpty @NotBlank String description;
+    @PositiveOrZero Long priority;
+    @Past LocalDateTime createdOn;
+}
