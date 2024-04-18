2e47c388ea7433d92357594b1dacc42d0e5faba7
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-18 11:41:10 +0200

    build: create account class

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/Account.java
@@ -0,0 +1,25 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.NaturalId;
+
+@Data
+@Embeddable
+public class Account {
+
+    @NotNull
+    @NaturalId
+    @Column(updatable = false, nullable = false)
+    private String accNumber;
+
+    @ColumnDefault("0.00")
+    @Column(nullable = false)
+    @NotNull
+    private BigDecimal balance;
+}
