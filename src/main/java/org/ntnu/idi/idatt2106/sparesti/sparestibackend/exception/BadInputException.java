3c2d157335b8e41cf7499d01f5e1a19ec8c69203
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-18 13:05:06 +0200

    Revert "Merge branch 'build/badge' into 'main'"
    This reverts merge request !8

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/exception/BadInputException.java
+++
@@ -1,8 +0,0 @@
-package org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception;
-
-public class BadInputException extends RuntimeException {
-
-    public BadInputException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.enums;
-
-public enum Role {
-    USER,
-    ADMIN,
-}
