e67aa28eba0f8098454404667e7661cc67985398
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-18 10:09:37 +0200

    build: add new challengetype enum

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/enums/ChallengeType.java
@@ -0,0 +1,10 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.enums;
+
+public enum ChallengeType {
+    CIGAR,
+    CLOTHING,
+    COFFEE,
+    SNUFF,
+    SNACKS,
+    TRANSPORTATION
+}
