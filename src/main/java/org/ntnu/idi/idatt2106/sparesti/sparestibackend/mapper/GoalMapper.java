f05928f8a14beb20ff041a3493682da542d64635
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-18 15:19:39 +0200

    build: implement mappers

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/mapper/GoalMapper.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/mapper/GoalMapper.java
@@ -1,8 +1,25 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.GoalDTO;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.Goal;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.util.ApplicationUtil;
 
+@Mapper(componentModel = "spring", imports = ApplicationUtil.class)
 public interface GoalMapper {
 
-    public static final GoalMapper INSTANCE = Mappers.getMapper(GoalMapper.class);
+    GoalMapper INSTANCE = Mappers.getMapper(GoalMapper.class);
+
+    @Mapping(
+            target = "completion",
+            expression = "java( ApplicationUtil.percentage(goal.getSaved(), goal.getTarget()) )")
+    GoalDTO toDTO(Goal goal);
+
+    @Mapping(
+            target = "completion",
+            expression =
+                    "java( ApplicationUtil.percentage(goalDTO.getSaved(), goalDTO.getTarget()) )")
+    Goal toEntity(GoalDTO goalDTO);
 }
