3c8e7ce0ea32abdd1c41d78d227906e0d1c16c2a
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-18 10:09:13 +0200

    fix: format

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/UserConfig.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/UserConfig.java
@@ -13,7 +13,7 @@ import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.enums.Role;
 @AllArgsConstructor
 @Data
 public class UserConfig {
-  private Motivation motivation;
-  private Role role;
-  private Experience experience;
+    private Motivation motivation;
+    private Role role;
+    private Experience experience;
 }
