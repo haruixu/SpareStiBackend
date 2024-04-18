ee1399bedcbe6283c4923f18844e3c5111236509
Harry Linrui XU <xulr0820@hotmail.com>
2024-04-18 11:52:37 +0200

    refactor: format

--- src/test/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/SpareStiBackendApplicationTests.java
+++ src/test/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/SpareStiBackendApplicationTests.java
@@ -1,14 +1,20 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import uk.org.webcompere.systemstubs.rules.EnvironmentVariablesRule;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.yml")
 @ActiveProfiles("test")
 class SpareStiBackendApplicationTests {
+
+    @Rule
+    public EnvironmentVariablesRule environmentVariablesRule =
+            new EnvironmentVariablesRule("SYSTEM_KEY", "12345");
 
     @Test
     void contextLoads() {}
