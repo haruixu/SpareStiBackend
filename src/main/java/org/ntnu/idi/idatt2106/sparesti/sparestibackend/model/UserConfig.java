24c5ad569a8b8474b83521a3af0e32d656f3865f
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-18 10:19:55 +0200

    build: add unique constraints

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/UserConfig.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/model/UserConfig.java
@@ -1,5 +1,6 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.enums.Experience;
@@ -13,7 +14,12 @@ import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.enums.Role;
 @AllArgsConstructor
 @Data
 public class UserConfig {
+    @Column(nullable = false)
     private Motivation motivation;
+
+    @Column(nullable = false)
     private Role role;
+
+    @Column(nullable = false)
     private Experience experience;
 }
