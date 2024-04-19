747820e533d1e0e1a0721ee5b50e1d2d2063bb33
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-19 13:53:07 +0200

    build: implement UserConfigService

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/service/UserConfigService.java
@@ -0,0 +1,28 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.controller.UserConfigResponse;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.ConfigNotFoundException;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.UserNotFoundException;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.mapper.UserConfigMapper;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.User;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserConfigService {
+
+    private final UserRepository userRepository;
+
+    public UserConfigResponse getUserConfig(Long id)
+            throws UserNotFoundException, ConfigNotFoundException {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+
+        if (user.getUserConfig() == null) {
+            throw new ConfigNotFoundException("No user config found for user with id " + id);
+        }
+
+        return UserConfigMapper.INSTANCE.userConfigToUserConfigResponse(user.getUserConfig());
+    }
+}
