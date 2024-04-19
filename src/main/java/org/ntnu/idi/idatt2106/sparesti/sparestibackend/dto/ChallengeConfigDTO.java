2c08df341653845662fba603cf6be2d274ad66d3
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-19 11:47:32 +0200

    build: new challengetypeconfigdto

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/dto/ChallengeConfigDTO.java
+++
@@ -1,7 +0,0 @@
-package org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto;
-
-/**
- * DTO for {@link org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.ChallengeConfig}
- */
-public class ChallengeConfigDTO {
-  }
---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/dto/ChallengeTypeConfigDTO.java
@@ -0,0 +1,20 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import lombok.Value;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.enums.ChallengeType;
+
+/**
+ * DTO for {@link org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.ChallengeTypeConfig}
+ */
+@Value
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ChallengeTypeConfigDTO implements Serializable {
+    @NotNull ChallengeType type;
+    @NotNull @PositiveOrZero BigDecimal generalAmount;
+    @NotNull @PositiveOrZero BigDecimal specificAmount;
+}
