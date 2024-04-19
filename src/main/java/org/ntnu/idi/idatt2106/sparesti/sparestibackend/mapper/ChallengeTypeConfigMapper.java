dbd4b036cea24f4f689221a250134032699e64b0
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-19 16:13:27 +0200

    fix: add INSTANCE field to mapper

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/mapper/ChallengeTypeConfigMapper.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/mapper/ChallengeTypeConfigMapper.java
@@ -2,11 +2,14 @@ package org.ntnu.idi.idatt2106.sparesti.sparestibackend.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.ChallengeTypeConfigDTO;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.ChallengeTypeConfig;
 
 @Mapper(componentModel = "spring")
 public interface ChallengeTypeConfigMapper {
+
+    ChallengeTypeConfigMapper INSTANCE = Mappers.getMapper(ChallengeTypeConfigMapper.class);
 
     @Mapping(target = "type", source = "type")
     @Mapping(target = "specificAmount", source = "specificAmount")
