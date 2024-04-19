27a63c26e2cf3ff99ea3cf85cd3fc81c2e150f73
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-19 11:42:39 +0200

    fix: remove hardcoded values for challengetypes

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/dto/ChallengeConfigDTO.java
@@ -0,0 +1,7 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto;
+
+/**
+ * DTO for {@link org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.ChallengeConfig}
+ */
+public class ChallengeConfigDTO {
+  }
