fe9a2041613c1729576fa316c76b165785ac7476
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-19 13:49:52 +0200

    fix: format

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/exception/UserNotFoundException.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
-       super("User with id " + id + " not found");
+        super("User with id " + id + " not found");
     }
 }
