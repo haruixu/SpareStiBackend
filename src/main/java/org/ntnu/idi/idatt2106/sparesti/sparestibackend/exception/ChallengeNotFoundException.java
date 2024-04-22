8712d1c4d59966ad71d6f72fb38de93da89b0444
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-22 15:55:35 +0200

    chore: implement challengenotfoundexception

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/exception/ChallengeNotFoundException.java
@@ -0,0 +1,7 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception;
+
+public class ChallengeNotFoundException extends RuntimeException {
+    public ChallengeNotFoundException(String message) {
+        super(message);
+    }
+}
