f036b67d150c99e9d259a92731e1b698122f7250
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-23 14:16:59 +0200

    chore: add methods that serve business logic for challenge config

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/service/UserConfigService.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/service/UserConfigService.java
@@ -1,10 +1,18 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.service;
 
 import lombok.RequiredArgsConstructor;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.ChallengeConfigDTO;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.ChallengeTypeConfigDTO;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.UserConfigDTO;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.ChallengeConfigNotFoundException;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.ChallengeTypeConfigNotFoundException;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.ConfigNotFoundException;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.UserNotFoundException;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.mapper.ChallengeConfigMapper;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.mapper.ChallengeTypeConfigMapper;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.mapper.UserConfigMapper;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.ChallengeConfig;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.ChallengeTypeConfig;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.User;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.UserConfig;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.repository.UserRepository;
@@ -36,6 +44,94 @@ public class UserConfigService {
         userRepository.save(user);
 
         return UserConfigMapper.INSTANCE.toDTO(newConfig);
+    }
+
+    public ChallengeConfigDTO createChallengeConfig(
+            String username, ChallengeConfigDTO challengeConfigDTO) throws UserNotFoundException {
+        User user = findUserByUsername(username);
+        ChallengeConfig challengeConfig =
+                ChallengeConfigMapper.INSTANCE.toEntity(challengeConfigDTO);
+        user.getUserConfig().setChallengeConfig(challengeConfig);
+
+        User persistedUser = userRepository.save(user);
+
+        return ChallengeConfigMapper.INSTANCE.toDTO(
+                persistedUser.getUserConfig().getChallengeConfig());
+    }
+
+    public ChallengeConfigDTO getChallengeConfig(String username)
+            throws ChallengeConfigNotFoundException, UserNotFoundException {
+        User user = findUserByUsername(username);
+        ChallengeConfig challengeConfig = user.getUserConfig().getChallengeConfig();
+
+        if (challengeConfig == null) {
+            throw new ChallengeConfigNotFoundException(user.getId());
+        }
+
+        return ChallengeConfigMapper.INSTANCE.toDTO(challengeConfig);
+    }
+
+    /**
+     * Method will reset all challenge type configs. make sure to send intact challenge type configs in the dto.
+     * This behaviour can be disabled, by ignoring the challenge type dtos in the mapper.
+     */
+    public ChallengeConfigDTO updateChallengeConfig(
+            String username, ChallengeConfigDTO challengeConfigDTO) throws UserNotFoundException {
+        User user = findUserByUsername(username);
+
+        ChallengeConfig challengeConfig = user.getUserConfig().getChallengeConfig();
+        ChallengeConfig updatedChallengeConfig =
+                ChallengeConfigMapper.INSTANCE.updateEntity(challengeConfig, challengeConfigDTO);
+        user.getUserConfig().setChallengeConfig(updatedChallengeConfig);
+        userRepository.save(user);
+
+        return ChallengeConfigMapper.INSTANCE.toDTO(updatedChallengeConfig);
+    }
+
+    public ChallengeTypeConfigDTO createChallengeTypeConfig(
+            String username, ChallengeTypeConfigDTO challengeTypeConfigDTO) {
+        User user = findUserByUsername(username);
+
+        ChallengeTypeConfig newConfig =
+                ChallengeTypeConfigMapper.INSTANCE.toEntity(challengeTypeConfigDTO);
+        user.getUserConfig().getChallengeConfig().getChallengeTypeConfigs().add(newConfig);
+
+        userRepository.save(user);
+
+        return ChallengeTypeConfigMapper.INSTANCE.toDTO(newConfig);
+    }
+
+    public ChallengeTypeConfigDTO updateChallengeTypeConfig(
+            String username, ChallengeTypeConfigDTO challengeTypeConfigDTO) {
+        final User user = findUserByUsername(username);
+        final String type = challengeTypeConfigDTO.type();
+
+        ChallengeTypeConfig oldConfig =
+                user.getUserConfig().getChallengeConfig().getChallengeTypeConfigs().stream()
+                        .filter(config -> config.getType().equalsIgnoreCase(type))
+                        .findFirst()
+                        .orElseThrow(() -> new ChallengeTypeConfigNotFoundException(type));
+
+        user.getUserConfig().getChallengeConfig().getChallengeTypeConfigs().remove(oldConfig);
+        ChallengeTypeConfig updatedConfig =
+                ChallengeTypeConfigMapper.INSTANCE.updateEntity(oldConfig, challengeTypeConfigDTO);
+        // maybe we have to do the latter
+        // user.getUserConfig().getChallengeConfig().getChallengeTypeConfigs().remove(oldConfig);
+        // user.getUserConfig().getChallengeConfig().getChallengeTypeConfigs().add(updatedConfig);
+        userRepository.save(user);
+        return ChallengeTypeConfigMapper.INSTANCE.toDTO(updatedConfig);
+    }
+
+    public void deleteChallengeTypeConfig(String type, String username)
+            throws UserNotFoundException, ChallengeTypeConfigNotFoundException {
+        User user = findUserByUsername(username);
+        ChallengeTypeConfig config =
+                user.getUserConfig().getChallengeConfig().getChallengeTypeConfigs().stream()
+                        .filter(_config -> _config.getType().equalsIgnoreCase(type))
+                        .findFirst()
+                        .orElseThrow(() -> new ChallengeTypeConfigNotFoundException(type));
+
+        user.getUserConfig().getChallengeConfig().getChallengeTypeConfigs().remove(config);
     }
 
     private User findUserByUsername(String username) throws UserNotFoundException {
