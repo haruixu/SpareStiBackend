52a24048dacfa32660ccdbace5bd7bb5b9dfad62
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-23 15:01:30 +0200

    fix: remove post mapping in user config

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/controller/UserConfigController.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/controller/UserConfigController.java
@@ -1,16 +1,12 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.controller;
 
-import jakarta.validation.Valid;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.UserConfigDTO;
-import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.BadInputException;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.ConfigNotFoundException;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.UserNotFoundException;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.service.UserConfigService;
-import org.ntnu.idi.idatt2106.sparesti.sparestibackend.util.ApplicationUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -32,17 +28,18 @@ public class UserConfigController {
         return ResponseEntity.ok(userConfig);
     }
 
-    @PostMapping
-    public ResponseEntity<UserConfigDTO> postUserConfig(
-            @Valid @RequestBody UserConfigDTO userConfigDTO,
-            BindingResult bindingResult,
-            @AuthenticationPrincipal UserDetails userDetails)
-            throws UserNotFoundException, BadInputException {
-        if (bindingResult.hasErrors()) {
-            throw new BadInputException(ApplicationUtil.BINDING_RESULT_ERROR);
-        }
-        UserConfigDTO newConfig =
-                userConfigService.createUserConfig(userDetails.getUsername(), userConfigDTO);
-        return ResponseEntity.ok(newConfig);
-    }
+    // NB! USER CONFIG IS CREATED FROM BEFORE. SEE CHALLENGE CONFIG TO EDIT CHALLENGE CONFIG
+    //@PostMapping
+    //public ResponseEntity<UserConfigDTO> postUserConfig(
+    //        @Valid @RequestBody UserConfigDTO userConfigDTO,
+    //        BindingResult bindingResult,
+    //        @AuthenticationPrincipal UserDetails userDetails)
+    //        throws UserNotFoundException, BadInputException {
+    //    if (bindingResult.hasErrors()) {
+    //        throw new BadInputException(ApplicationUtil.BINDING_RESULT_ERROR);
+    //    }
+    //    UserConfigDTO newConfig =
+    //            userConfigService.createUserConfig(userDetails.getUsername(), userConfigDTO);
+    //    return ResponseEntity.ok(newConfig);
+    //}
 }
