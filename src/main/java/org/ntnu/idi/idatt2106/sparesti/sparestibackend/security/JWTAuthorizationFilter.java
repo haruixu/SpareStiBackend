96f67e7baacd603130942f518d7743f4e60b9b2a
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-23 19:59:14 +0200

    fix: print stacktrace

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/security/JWTAuthorizationFilter.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/security/JWTAuthorizationFilter.java
@@ -83,7 +83,8 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             }
             filterChain.doFilter(request, response);
         } catch (JwtException | ServletException e) {
-            logger.error("Caught exception in filter");
+            // TODO: remove e.getMessage() before delivery, only for debugging
+            logger.error("Caught exception in filter: {}", e.getStackTrace());
             String responseMsg = "Invalid or expired JWT token";
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             response.getWriter().write(responseMsg);
