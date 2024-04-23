303b3360f51333181ad17d0fd05d89e590813b6f
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-23 14:06:20 +0200

    chore: add exceptions for config of challenge

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/exception/ChallengeConfigNotFoundException.java
@@ -0,0 +1,7 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception;
+
+public class ChallengeConfigNotFoundException extends RuntimeException {
+    public ChallengeConfigNotFoundException(Long id) {
+        super("Could not find challenge config for user with id " + id);
+    }
+}
