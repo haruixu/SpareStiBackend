82848972db319a0f8429cf690a645a0fc9c7f378
lmlnilse <lmlnilse@stud.ntnu.no>
2024-04-24 14:29:57 +0200

    fix: Copied JWTAuthorizationFilter from another branch

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/security/JWTAuthorizationFilter.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/security/JWTAuthorizationFilter.java
@@ -1,11 +1,15 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.security;
 
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -27,6 +31,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
     private final JWTService jwtService;
     private final UserDetailsService userDetailsService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     /**
      * The JWT filter
@@ -42,6 +47,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             @NotNull FilterChain filterChain)
             throws IOException {
         try {
+            logger.info("Checking 'Authorization' header");
             final String authHeader = request.getHeader("Authorization");
             final String jwt;
             final String username;
@@ -49,16 +55,22 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
                 filterChain.doFilter(request, response);
                 return;
             }
+            logger.info("Extracting token");
             jwt = authHeader.substring(7);
+            logger.info("Extracting username from token");
             username = jwtService.extractUsername(jwt);
             // Checks if user is not null and that it has not already been authenticated - else
             // there is
             // no point in re-authenticating
+            logger.info(
+                    "Checking that username is not null and that it hasn't been authenticated yet");
             if (username != null
                     && SecurityContextHolder.getContext().getAuthentication() == null) {
+                logger.info("Finding user");
                 // Retrieve user details from database for validation
                 UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
                 // If token is valid, update security context
+                logger.info("Checking token validity");
                 if (jwtService.isTokenValid(jwt, userDetails)) {
                     UsernamePasswordAuthenticationToken authToken =
                             new UsernamePasswordAuthenticationToken(
@@ -66,12 +78,14 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
                     authToken.setDetails(
                             new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authToken);
+                    logger.info("Successfully authenticated user");
                 }
             }
             filterChain.doFilter(request, response);
-        } catch (Exception e) {
+        } catch (JwtException | ServletException e) {
+            logger.error("Caught exception in filter: {}", e.getMessage());
             String responseMsg = "Invalid or expired JWT token";
-            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
             response.getWriter().write(responseMsg);
         }
     }
