2b80a11fe65e42c9cbcf1015aff0d5e4b4443279
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-24 15:45:58 +0200

    chore: implement business logic for new endpoints

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/service/ChallengeService.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/service/ChallengeService.java
@@ -1,9 +1,11 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.service;
 
+import java.time.ZonedDateTime;
 import lombok.RequiredArgsConstructor;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.challenge.ChallengeCreateDTO;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.challenge.ChallengeDTO;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.challenge.ChallengeUpdateDTO;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.ChallengeAlreadyCompletedException;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.exception.ChallengeNotFoundException;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.mapper.ChallengeMapper;
 import org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.Challenge;
@@ -22,6 +24,34 @@ public class ChallengeService {
     public Page<ChallengeDTO> getChallengesByUser(User user, Pageable pageable)
             throws ChallengeNotFoundException {
         return challengeRepository.findByUser(user, pageable).map(ChallengeMapper.INSTANCE::toDTO);
+    }
+
+    public Page<ChallengeDTO> getActiveChallenges(User user, Pageable pageable) {
+        return challengeRepository
+                .findAllByCompletedOnIsNullAndUser(user, pageable)
+                .map(ChallengeMapper.INSTANCE::toDTO);
+    }
+
+    public Page<ChallengeDTO> getCompletedChallenges(User user, Pageable pageable) {
+        return challengeRepository
+                .findAllByCompletedOnIsNotNullAndUser(user, pageable)
+                .map(ChallengeMapper.INSTANCE::toDTO);
+    }
+
+    public ChallengeDTO completeChallenge(Long challengeId, User user) {
+        Challenge challenge = findChallengeByIdAndUser(challengeId, user);
+        if (challenge.getCompletedOn() != null) {
+            throw new ChallengeAlreadyCompletedException(challengeId);
+        }
+        challenge.setCompletedOn(ZonedDateTime.now());
+        challengeRepository.save(challenge);
+        return ChallengeMapper.INSTANCE.toDTO(challenge);
+    }
+
+    private Challenge findChallengeByIdAndUser(Long challengeId, User user) {
+        return challengeRepository
+                .findByIdAndUser(challengeId, user)
+                .orElseThrow(() -> new ChallengeNotFoundException(challengeId));
     }
 
     public ChallengeDTO save(ChallengeCreateDTO challengeCreateDTO, User user)
