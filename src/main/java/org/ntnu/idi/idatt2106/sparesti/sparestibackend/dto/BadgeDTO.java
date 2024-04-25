607b31f66ac3ecdaeec2192eb549a2c6937cdc1a
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-25 12:30:22 +0200

    chore: implement user and badge dtos

---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/dto/BadgeDTO.java
@@ -0,0 +1,18 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.io.Serializable;
+
+/**
+ * DTO for {@link org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.Badge}
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record BadgeDTO(
+        @NotNull @Positive Long id,
+        @NotNull @NotEmpty @NotBlank String title,
+        @NotNull @NotEmpty @NotBlank String description)
+        implements Serializable {}
---
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/dto/user/UserResponse.java
@@ -0,0 +1,24 @@
+package org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Set;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.AccountDTO;
+import org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.BadgeDTO;
+
+/**
+ * DTO for {@link org.ntnu.idi.idatt2106.sparesti.sparestibackend.model.User}
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record UserResponse(
+        Long id,
+        @NotNull String firstName,
+        @NotNull String lastName,
+        @NotNull String username,
+        @NotNull @Email String email,
+        @NotNull AccountDTO spendingAccount,
+        @NotNull AccountDTO savingAccount,
+        @NotNull Set<BadgeDTO> badges)
+        implements Serializable {}
