eccda1e1e6e1d62b82e1d3dd323d02c7359c7586
Harry Linrui XU <xulr0820@hotmail.com>
2024-04-26 12:08:19 +0200

    refactor: format

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/exception/GlobalExceptionHandler.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/exception/GlobalExceptionHandler.java
@@ -17,7 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
