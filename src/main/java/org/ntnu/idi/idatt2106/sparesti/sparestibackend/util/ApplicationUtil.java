fd62081012e9290f45bdbe5e38526f9a78d68fc5
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-28 22:34:39 +0200

    chore: add new find random byte array generator

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/util/ApplicationUtil.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/util/ApplicationUtil.java
@@ -1,17 +1,26 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.util;
 
+import com.yubico.webauthn.data.ByteArray;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.security.SecureRandom;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationUtil {
 
     public static final BigDecimal ONE_HUNDRED = new BigDecimal(100);
-    public static final RoundingMode roundingMode = RoundingMode.HALF_UP;
-    private static final int scale = 3;
+    public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
+    private static final int ROUNDING_SCALE = 3;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static ByteArray generateRandom(int length) {
+        byte[] bytes = new byte[length];
+        random.nextBytes(bytes);
+        return new ByteArray(bytes);
+    }
 
     public static BigDecimal percent(BigDecimal quot, BigDecimal divisor) {
-        return quot.multiply(ONE_HUNDRED).divide(divisor, scale, roundingMode);
+        return quot.multiply(ONE_HUNDRED).divide(divisor, ROUNDING_SCALE, ROUNDING_MODE);
     }
 }
