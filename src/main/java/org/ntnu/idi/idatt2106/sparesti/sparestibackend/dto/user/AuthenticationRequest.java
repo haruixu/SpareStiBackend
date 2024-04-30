c9ea29b5fe0006ec02d013151530259fc9547736
Harry Linrui XU <xulr0820@hotmail.com>
2024-04-30 11:09:58 +0200

    fix: Imporve error message

--- src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/dto/user/AuthenticationRequest.java
+++ src/main/java/org/ntnu/idi/idatt2106/sparesti/sparestibackend/dto/user/AuthenticationRequest.java
@@ -1,17 +1,9 @@
 package org.ntnu.idi.idatt2106.sparesti.sparestibackend.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 
 public record AuthenticationRequest(
-        @NotNull(message = "Username cannot be null")
-                @NotBlank(message = "Username cannot be blank")
-                @NotEmpty(message = "Username cannot be empty")
-                String username,
-        @NotNull(message = "Password cannot be null")
-                @NotBlank(message = "Password cannot be blank")
-                @NotEmpty(message = "Password cannot be empty")
-                String password)
+        @NotBlank(message = "Brukernavn eller passord er feil") String username,
+        @NotBlank(message = "Brukernavn eller passord er feil") String password)
         implements Serializable {}
